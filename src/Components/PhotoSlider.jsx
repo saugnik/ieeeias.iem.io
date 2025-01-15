3ab750475ec30acc469f3fd26ab9578d8c2ebbb1
saugnik <148228363+saugnik@users.noreply.github.com>
2025-01-15 21:54:39 +0530

    Update PhotoSlider.jsx

--- src/Components/PhotoSlider.jsx
+++ src/Components/PhotoSlider.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './PhotoSlider.css';
-// import './../public/images/event_pic1.jpg';
 
 const PhotoSlider = () => {
   const images = [
@@ -9,21 +8,19 @@ const PhotoSlider = () => {
     "event_pic3.jpg",
     "event_pic4.jpg",
     "event_pic5.jpg",
-    "event_pic 6.jpg",
-    "event_pic 7.jpg"
+    "event_pic6.jpg",
+    "event_pic7.jpg"
   ];
-
-  
 
   return (
     <section id="photos" className="photos-section mx-auto my-5 p-5">
       <h2>Photo Slider</h2>
       <div className="slider-container relative overflow-hidden rounded-lg shadow-lg">
         <div className="slider-track flex w-full h-[400px]">
-          {images.map((images, index) => (
+          {images.map((image, index) => (
             <div key={index} className="slide">
               <img 
-                src={`${images}`} 
+                src={`./Images/${image}`} 
                 alt={`Slide ${index + 1}`} 
                 className="w-full h-full object-cover"
               />
