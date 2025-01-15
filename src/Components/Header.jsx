aa4a9a9142c5e69c6d006cc890f0a07317f35029
saugnik <148228363+saugnik@users.noreply.github.com>
2025-01-15 22:19:29 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-r from-[#004085] to-[#2a5faa] text-white p-5 shadow-md flex justify-between items-center sticky top-0 z-[1000]">
       <div className="logo-container">
-        <img src="th (1).jpeg" alt="IEEE-IAS Logo" className="w-[100px]" />
+        <img src="src\public\Images\th (1).jpeg" alt="IEEE-IAS Logo" className="w-[100px]" />
       </div>
       <div className="header-title flex-grow text-center">
         <h1 className="text-2xl">IEEE-IAS SBC, IEM (Kolkata)</h1>
       </div>
       <div className="logo-container">
-        <img src="th.jpeg" alt="IEM Logo" className="w-[100px]" />
+        <img src="src\public\Images\th.jpeg" alt="IEM Logo" className="w-[100px]" />
       </div>
     </header>
   );
