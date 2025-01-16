10bf1888cdb0763b3f13d6cffd0058bfe83e759a
saugnik <148228363+saugnik@users.noreply.github.com>
2025-01-16 20:33:44 +0530

    Update Advisor.jsx

--- src/Components/Advisor/Advisor.jsx
+++ src/Components/Advisor/Advisor.jsx
@@ -12,7 +12,7 @@ const Advisor = () => {
           </div>
           <li><strong>Web Master:</strong> Himangshu Singh</li>
           <li><strong>Advisor:</strong> Dr. Prof Sourav Das</li>
-          <p>[A brief description of the advisor’s background, role, and contributions to the club.]</p>
+          <p>[The Institute of Electrical and Electronics Engineers (IEEE)-Industry Application society (IAS) Student Branch Chapter (SBC)(IEEE-IAS SBC, Code: IA14301D) started its journey in 2024 and is currently coordinated under the active leadership of Prof. Dr. Sourav Das, department of Electrical Engineering, IEM Kolkata. Our chapter envisions a future where electrical engineering students from IEM Kolkata are at the forefront of technological innovation, making significant contributions to our society. We aspire to create a platform that nurtures creativity, critical thinking, and a lifelong passion for learning. Our mission is to empower students with the knowledge, skills, and opportunities to excel in their academic and professional pursuits. We aim to bridge the gap between academia and industry by fostering collaborations and providing practical exposure to real-world challenges.]</p>
         </div>
       </div>
     </section>
