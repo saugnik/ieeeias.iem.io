26c9718212f1cd0a5122848ed480883a482d703d
saugnik <148228363+saugnik@users.noreply.github.com>
2025-01-16 20:27:13 +0530

    /

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,13 +5,13 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <img src="./Images/th.jpeg" alt="IEEE-IAS Logo" />
+        <img src="./Images/th.jpeg" alt="IEEE-IAS Logo" className={styles.logo} />
       </div>
       <div className={styles.headerTitle}>
         <h1>IEEE-IAS SBC, IEM (Kolkata)</h1>
       </div>
       <div className={styles.logoContainer}>
-        <img src="./Images/th (1).jpeg" alt="IEM Logo" />
+        <img src="./Images/th (1).jpeg" alt="IEM Logo" className={styles.logo} />
       </div>
     </header>
   );
